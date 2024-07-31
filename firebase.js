646389ecc851d03053bd41d5b305e7a49503f94e
Syed Taha <syetaha@gmail.com>
2024-08-01 02:08:32 +0500

    update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from 'firebase/auth';
+
 
 // Your web app's Firebase configuration from environment variables
 const firebaseConfig = {
@@ -11,23 +11,13 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID // This can be removed if not using Analytics
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
-const auth = getAuth(app);
 
-// Conditionally initialize Firebase Analytics only on the client side
-let analytics;
-if (typeof window !== 'undefined') {
-  const { getAnalytics, isSupported } = require('firebase/analytics');
-  isSupported().then(supported => {
-    if (supported) {
-      analytics = getAnalytics(app);
-    }
-  });
-}
 
-export { app, firestore, auth, analytics };
+// Export the initialized Firebase services
+export { app, firestore};
